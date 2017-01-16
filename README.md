c30ae7cdf3ec423722e3f856abda47967007d764
Liang Xingchen <liang@maichong.it>
2017-01-16 21:14:24 +0800

    0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # shallow-equal-without
 Shallow equal check object without specified properties.
+
+```js
+
+import shallowEqualWithout from 'shallow-equal-without';
+
+let a = { foo: 123, bar:'string', baz:true };
+let b = { foo: 123, bar:'string'};
+
+shallowEqualWithout(a,b,'baz'); // true
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * @copyright Maichong Software Ltd. 2017 http://maichong.it
+ * @date 2017-01-16
+ * @author Liang <liang@maichong.it>
+ */
+
+var _ = require('lodash');
+
+module.exports = function shallowEqualWithout(objA, objB) {
+  if (objA === objB) {
+    return true;
+  }
+
+  if (typeof objA !== 'object' || objA === null ||
+    typeof objB !== 'object' || objB === null) {
+    return false;
+  }
+
+  var excludes = _.slice(arguments, 2);
+  var keysA = _.without.apply(_, [_.keys(objA)].concat(excludes));
+  var keysB = _.without.apply(_, [_.keys(objB)].concat(excludes));
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (var i = 0; i < keysA.length; i++) {
+    if (!objB.hasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
+      return false;
+    }
+  }
+
+  return true;
+};
